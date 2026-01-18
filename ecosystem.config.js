7bc397672b407f8322affb0f01f59197b6ff6c33
fazzaria <fazzaria@yahoo.com>
2026-01-18 17:20:48 -0500

    Hopefully last config fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,9 @@
-module.exports = {
-  apps: [
-    {
-      name: "poem-bot",
-      script: "src/server/index.ts",
-      interpreter: "node",
-      interpreter_args: [
-        "-r",
-        "ts-node/register",
-        "-r",
-        "tsconfig-paths/register",
-      ],
-      instances: 1,
-      watch: false,
-    },
-  ],
-};
+export const apps = [
+  {
+    name: "poem-bot",
+    script: "dist/index.cjs",
+    interpreter: "node",
+    instances: 1,
+    watch: false,
+  },
+];
